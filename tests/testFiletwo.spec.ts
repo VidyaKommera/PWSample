161715ba0553f03a4547374686bd7ca044df60ae
VidyaKommera <vidyakommeraexternal@gmail.com>
2026-05-21 11:34:13 +0530

    phase2  push

--- tests/testFiletwo.spec.ts
+++ tests/testFiletwo.spec.ts
@@ -2,4 +2,5 @@ import {test, expect} from '@playwright/test'
  test('Assertions test.....',{tag: '@SmokeTest'}, async({page})=>{
 const val=10
 expect(val).toBeGreaterThan(5)
+console.log("Phase2 push")
  })
