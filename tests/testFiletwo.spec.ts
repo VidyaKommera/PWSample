40cff1aa8b49af592e97d9aec57f3b7806b2d231
VidyaKommera <vidyakommeraexternal@gmail.com>
2026-05-21 11:27:47 +0530

    Main push

--- tests/testFiletwo.spec.ts
+++ tests/testFiletwo.spec.ts
@@ -2,4 +2,6 @@ import {test, expect} from '@playwright/test'
  test('Assertions test.....',{tag: '@SmokeTest'}, async({page})=>{
 const val=10
 expect(val).toBeGreaterThan(5)
+ console.log('main push')
  })
+
